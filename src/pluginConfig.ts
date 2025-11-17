237d455d9b0fd9b208c8cbf22cc88ede7f4ad569
Thomas Cailhol <cailhol.thomas@gmail.com>
2025-11-17 13:07:08 +0100

    increm versions

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-the-buoy',
-    version: '0.1.1',
+    version: '0.1.2',
     icon: '🌊',
     title: 'The Buoy',
     description:
