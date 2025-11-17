5c37a39cbe98cdb590cc50b69fc8ed7e42ddaf15
Thomas Cailhol <cailhol.thomas@gmail.com>
2025-11-17 13:19:29 +0100

    use https

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-the-buoy',
-    version: '0.1.2',
+    version: '0.1.3',
     icon: '🌊',
     title: 'The Buoy',
     description:
